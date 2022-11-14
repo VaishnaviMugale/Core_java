0669841f9b49464b8ad1e184902b04a7fcab6409
VaishnaviMugale <117432327+VaishnaviMugale@users.noreply.github.com>
2022-11-14 10:40:57 +0530

    Add files via upload

---
+++ Operators/RalationalOperators.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class RalationalOperators {
+
+	public static void main(String[] args) {
+		// <,>,<=,>=,!=,==
+		System.out.println(9<10);
+		System.out.println(9>10);
+		
+		int a=100,b=200;
+		System.out.println(a<b);
+		System.out.println(a!=b);
+		System.out.println(a==b);
+		
+	}
+
+}
